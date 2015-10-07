07fdd2f04043456f089f02bf0a2fb8f74ea0ada4
Teppo Rekola <sytem@iki.fi>
2015-10-07 20:18:41 +0300

    ad rotate-template

---
+++ adrotate.js
@@ -0,0 +1,10 @@
+//from: http://snook.ca/archives/javascript/simplest-jquery-slideshow by Jonathan Snook
+
+$(function(){
+    $('.fadein img:gt(0)').hide();
+    setInterval(function(){
+      $('.fadein :first-child').fadeOut()
+         .next('img').fadeIn()
+         .end().appendTo('.fadein');}, 
+      5000);
+});
