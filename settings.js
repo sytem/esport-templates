180a43c2e02d3b3c541bf415857c4bed8db54d24
Teppo Rekola <sytem@iki.fi>
2015-10-08 17:23:26 +0300

    first settings

---
+++ settings.js
@@ -0,0 +1,10 @@
+var settings = {
+	jsonurl = ""
+	
+	casters = { 
+		f0 = "name 1"
+		f1 = "nick 1"
+		f2 = "name 2"
+		f3 = "nick 2"
+	}	
+};	
--- testFrame.js
+++ testFrame.js
@@ -11,7 +11,8 @@ $(function() {
 	});
 
 	$( "#testUpdate" ).click(function() {
-	  document.getElementById("testbed").contentWindow.update(document.getElementById("updateString").value);
+	// ### need fix to same format caspar uses
+	  document.getElementById("testbed").contentWindow.update(escapeHtml(document.getElementById("updateString").value));
 	});
 
 	$( "#testNext" ).click(function() {
@@ -22,12 +23,22 @@ $(function() {
 	  document.getElementById("testbed").contentWindow.remove();
 	});
 
-	$( "#testInvoke" ).click(function() {
-	  document.getElementById("testbed").contentWindow.invoke(document.getElementById("invokeString").value);
-	});
+	// xss-hazard, enable only on private enviroment
+	// not possible to clean javascript because need to insert js to templates
+	//$( "#testInvoke" ).click(function() {
+	//  document.getElementById("testbed").contentWindow.invoke(document.getElementById("invokeString").value);
+	//});
 
 	$( "#templateSelect" ).change(function() {
   		$('#testbed').attr('src', document.getElementById("templateSelect").value);
 	});
 
 });	
+
+//from http://shebang.brandonmintern.com/foolproof-html-escaping-in-javascript/
+
+function escapeHtml(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+};
