6e25479ee7fcb876b59844bd32fea22725749a26
sytem <sytem@iki.fi>
2015-10-09 22:32:56 +0300

    find images in adrotate-folder(corss-problem, need to load remote files)

--- adrotate.js
+++ adrotate.js
@@ -1,6 +1,21 @@
-//from: http://snook.ca/archives/javascript/simplest-jquery-slideshow by Jonathan Snook
-
 $(function(){
+	
+	//http://stackoverflow.com/questions/18480550/how-to-load-all-the-images-from-one-of-my-folder-into-my-web-page-using-jquery
+	var dir = "http://sytem.fi/esports-templates/adrotate/";
+	var fileextension = [".png", ".jpg"];
+	$.ajax({
+		//This will retrieve the contents of the folder if the folder is configured as 'browsable'
+		url: dir,
+		success: function (data) {
+			//List all .png file names in the page
+			$(data).find("a:contains(" + (fileextension[0]) + "), a:contains(" + (fileextension[1]) + ")").each(function () {
+				var filename = this.href.replace(window.location.host, "").replace("http://", "");
+				$(".fadein").append("<img src='" + dir + filename + "'>");
+			});
+		}
+	});
+
+	//from: http://snook.ca/archives/javascript/simplest-jquery-slideshow by Jonathan Snook
     $('.fadein img:gt(0)').hide();
     setInterval(function(){
       $('.fadein :first-child').fadeOut()
@@ -8,3 +23,5 @@ $(function(){
          .end().appendTo('.fadein');}, 
       5000);
 });
+
+
